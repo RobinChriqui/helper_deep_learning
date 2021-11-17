d00bfaa86b51583e7268ac60973be328fc70eb0d
RobinChriqui <93709005+RobinChriqui@users.noreply.github.com>
2021-11-17 14:52:43 +0200

    Update helper_functions.py

--- helper_functions.py
+++ helper_functions.py
@@ -40,3 +40,12 @@ def plot_loss_curves(history):
   plt.title("Accrucacy")
   plt.xlabel("Epochs")
   plt.legend()
+
+
+def create_tensorboard_callback(dir_name, experiment_name):
+  log_dir=dir_name+ "/" + experiment_name + "/" + datetime.datetime.now().strftime("%Y%m%d-H%M%S")
+  tensorboard_callback = tf.keras.callbacks.TensorBoard(
+      log_dir=log_dir
+  )
+  print(f"Saving TensorBoard log files to:{log_dir}")
+  return tensorboard_callback
