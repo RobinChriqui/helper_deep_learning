d9192d1689ba5aa0986cfb3b9791c762cb25b9e5
RobinChriqui <93709005+RobinChriqui@users.noreply.github.com>
2021-11-18 17:11:56 +0200

    Update helper_functions.py

--- helper_functions.py
+++ helper_functions.py
@@ -81,3 +81,25 @@ def create_model(model_url, num_classes=10):
   ])
 
   return model
+
+# View an image
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import random
+
+def view_random_image(target_dir, target_class):
+  # Setup target directory (we'll view images from here)
+  target_folder = target_dir+target_class
+
+  # Get a random image path
+  random_image = random.sample(os.listdir(target_folder), 1)
+
+  # Read in the image and plot it using matplotlib
+  img = mpimg.imread(target_folder + "/" + random_image[0])
+  plt.imshow(img)
+  plt.title(target_class)
+  plt.axis("off");
+
+  print(f"Image shape: {img.shape}") # show the shape of the image
+
+  return img
